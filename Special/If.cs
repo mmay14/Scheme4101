6d409e8216cb8fd0bd3624a7e3a701122135691c
Jackie Robinson <jackierobinsonm@gmail.com>
2016-11-07 01:09:22 -0600

    Finshed if and started Unspecific
    renamed some of the variables in the if to make more sense. added
    comments.

--- Special/If.cs
+++ Special/If.cs
@@ -15,34 +15,29 @@ namespace Tree
             Printer.printIf(t, n, p);
         }
 
+        //syntax: if <test> <consequent> <alternate>
+        //syntax: if <test> <consequent>
+  
         public override Node eval(Node exp, Environment env)
         {
-            int num = Node.length(exp);
-            if (num != 3 || num != 4)
+            var length = Node.length(exp);
+            if (length != 3 || length != 4)
             {
-                Console.Error.WriteLine("Error: invalid if expression");
+                Console.Error.WriteLine("Error: Invalid length for if expression");
                 return Nil.getInstance();
             }
-            Node testExp = exp.getCdr().getCar();
-            Node trueExp = exp.getCdr().getCdr().getCar();
-            Node falseExp = new Node();
-            if (num == 4)
-            {
-                falseExp = exp.getCdr().getCdr().getCdr().getCar();
-            }
-            if (testExp.eval(env) == BoolLit.getInstance(true))
-            {
-                return trueExp.eval(env);
-            }
-            else if (testExp.eval(env) == BoolLit.getInstance(false) && num == 4)
-            {
-                return falseExp.eval(env);
-            }
-            else
-            {
-                // TODO: what to return if false and num = 3
-                // unspecified
-            }
+            var test = exp.getCdr().getCar();
+            var consequent = exp.getCdr().getCdr().getCar();
+
+            var alternate = new Node();
+            if (length == 4) //has alternates
+                alternate = exp.getCdr().getCdr().getCdr().getCar();
+            else //If <test> yields a false value and no <alternate> is specified, then the result of the expression is unspecified.
+                alternate = Unspecific.getInstance();
+
+            if (test.eval(env) == BoolLit.getInstance(true))
+                return consequent.eval(env);
+            return alternate.eval(env);
         }
     }
 }
